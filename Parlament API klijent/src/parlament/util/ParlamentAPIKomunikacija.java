2e668e4b2f0e3dfa76d4713d0447fec5d7c0b930
djordjenestorovic18214 <djordje_95@hotmail.com>
2016-05-14 19:17:05 +0200

    Napravljene klase Poslanik(implementirano) i ParlamentAPIKomunikacija.

---
+++ Parlament API klijent/src/parlament/util/ParlamentAPIKomunikacija.java
@@ -0,0 +1,14 @@
+package parlament.util;
+
+import java.util.LinkedList;
+
+import parlament.domain.Poslanik;
+
+public class ParlamentAPIKomunikacija {
+	
+	private static final String parlamentURL = "http://147.91.128.71:9090/parlament/api/members";
+//	public LinkedList<Poslanik> vratiPoslanike() {
+//		
+//		
+//	}
+}
